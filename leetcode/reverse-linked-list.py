3412374ac210e29c9fda6fb1fdf9f19670ec1e3e
Angeni Jerish <angeni.jerish@gmail.com>
2026-08-09 16:20:49 -0400

    Update solution for reverse-linked-list

--- leetcode/reverse-linked-list.py
+++ leetcode/reverse-linked-list.py
@@ -9,28 +9,13 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        
-        if(head==None):
-            return None
-        
-        cur_node=head  
-        old_list=None
-        
-        while(cur_node !=None and cur_node.next!=None):
-            
-            next_node=cur_node.next
-            next_next_nodes=cur_node.next.next   
-                
-                
-            next_node.next= cur_node
-            cur_node.next=old_list
-            old_list=next_node
-            
-            cur_node = next_next_nodes
-            
-        if(cur_node!=None):
-            cur_node.next = old_list
-        else:
-            cur_node=old_list
-        
-        return cur_node
+
+        visited=None
+        cur=head
+
+        while cur:
+            temp = cur.next
+            cur.next = visited
+            visited =cur
+            cur = temp
+        return visited
